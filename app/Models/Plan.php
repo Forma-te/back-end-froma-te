dfab6ac6f36ca0311b8a1ea625f1854f261da3cc
MoisesBumba <moises-alberto@hotmail.com>
2024-06-23 12:35:56 +0100

    create tables end model Plan, DetailPlan

---
+++ app/Models/Plan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'url', 'price', 'description', 'quantity','published'];
+
+    public function details()
+    {
+        return $this->hasMany(DetailPlan::class);
+    }
+}
